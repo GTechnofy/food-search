a21cfd4bea3f0cd103f30912340d44afb096067c
ggupta <grvgupta999@gmail.com>
2019-05-08 22:50:07 +0530

    Removing androidTest and Test and package restructuring

---
+++ app/src/main/java/com/gtechnofy/sample/FoodSearchActivity.java
@@ -0,0 +1,15 @@
+package com.gtechnofy.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gtechnofy.sample.foodsearch.R;
+
+public class FoodSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_search);
+    }
+}
